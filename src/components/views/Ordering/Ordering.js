2218f0aafd084399c2596734169d73791beece76
Konrad Lis <o.konradlis@gmail.com>
2019-12-24 00:19:14 +0100

    Install MaterialUI

--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -2,16 +2,21 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from '../Ordering/Ordering.scss'
+import { Button } from '@material-ui/core';
 
 const Ordering = () => (
     <div className={styles.component}>
         <h2>
             Ordering View
         </h2>
-        <nav>
+        <Button variant='outlined' size='small' color='secondary'>
+        
             <Link to={`${process.env.PUBLIC_URL}/Ordering/new`}>NewOrder</Link>
+        </Button>
+        <Button variant='outlined' size='small' color='secondary'>
             <Link to={`${process.env.PUBLIC_URL}/Ordering/order/ :id`}>Order id</Link>
-        </nav>
+       </Button>
+            
     </div>
 );
 
